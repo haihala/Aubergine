6547a278afa192a61ae8fa7d501ba15635820022
haihala <eero.haihala@gmail.com>
2018-02-04 04:43:54 +0200

    Fixed tilemap not colliding, added primitive player character

---
+++ Assets/Aubergine/Player/Scripts/WASD_mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_mover : MonoBehaviour {
+
+	Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
