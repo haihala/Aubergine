59b6fa9218db0938933367e42ae9bdf3c023dbf8
haihala <eero.haihala@gmail.com>
2018-02-04 06:38:38 +0200

    goddamn cracks in tilemaps

--- Assets/Aubergine/Player/Scripts/WASD_mover.cs
+++ Assets/Aubergine/Player/Scripts/WASD_mover.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WASD_mover : MonoBehaviour {
+	// Publics
+	public FloatVariable speed;
 
+	// Privates
 	Rigidbody2D rigidbody;
 
 	// Use this for initialization
@@ -13,6 +16,11 @@ public class WASD_mover : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		Vector2 Direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Direction.Normalize();
+		Direction *= speed.Value * Time.deltaTime;
+
+		rigidbody.AddForce(Direction);
 
 	}
 }
