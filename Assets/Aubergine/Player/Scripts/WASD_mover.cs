afd3dfbb64282e70eb30a400b6cd8d3c444e7b7c
haihala <eero.haihala@gmail.com>
2018-02-06 01:07:39 +0200

    dvorak binds and dashes (framework and implementation)

--- Assets/Aubergine/Player/Scripts/WASD_mover.cs
+++ Assets/Aubergine/Player/Scripts/WASD_mover.cs
@@ -2,25 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WASD_mover : MonoBehaviour {
 	// Publics
 	public FloatVariable speed;
+	public Dash dash;
 
 	// Privates
 	Rigidbody2D rigidbody;
 
+	// Indicates the phase of the dash, since velocity is not constant.
+	Vector2 dash_direction;
+	float dash_start;
+
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody2D>();
+		dash_start = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector2 Direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		Direction.Normalize();
-		Direction *= speed.Value * Time.deltaTime;
+		// Walking
+		Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		direction.Normalize();
+		direction *= speed.Value * Time.deltaTime;
 
-		rigidbody.AddForce(Direction);
+		rigidbody.AddForce(direction);
 
+
+		// Dash
+		if (Time.time - dash_start < dash.duration) {
+			// Dash is ongoing, continue
+			rigidbody.AddForce(dash_direction * dash.force * dash.power(dash_start));
+
+		} else if (Input.GetButton("Dash")) {
+			if (Time.time - dash_start > dash.cooldown) {
+				// Cooldown passed
+				start_dash();
+			}
+		}
+	}
+
+	void start_dash(){
+		dash_direction = new Vector2(Input.mousePosition.x - Screen.width/2F, Input.mousePosition.y - Screen.height/2F);
+		dash_direction.Normalize();
+
+		dash_start = Time.time;
 	}
 }
---
+++ Assets/Aubergine/Scriptables/Dash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Dash : ScriptableObject {
+	public float duration;
+	public float force;
+	public float cooldown;
+
+	abstract public float power(float start_time);
+}
+
+
+[CreateAssetMenu(menuName = "Aubergine/Dash/Basic")]
+public class Basic_Dash : Dash {
+	override public float power(float start_time) {
+		// returns constant 1
+		return 1f;
+	}
+}
+
+[CreateAssetMenu(menuName = "Aubergine/Dash/Linear")]
+public class Linear_Dash : Dash {
+	override public float power(float start_time) {
+		// returns constant 1
+		return (Time.time - start_time) / duration;
+	}
+}
+
+[CreateAssetMenu(menuName = "Aubergine/Dash/Reverse_Linear")]
+public class Reverse_Linear_Dash : Dash {
+	override public float power(float start_time) {
+		// returns constant 1
+		return (duration + start_time - Time.time) / duration;
+	}
+}
